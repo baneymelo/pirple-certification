ac025b4f9cfa093f4b9a075370b0711d40e3438b
Baney <baneymelo@gmail.com>
2020-05-16 22:15:15 -0500

    Homework Assignment #5: Switch Statements

--- ES6/switch.js
+++ ES6/switch.js
@@ -1,77 +1,131 @@
-/* const myFruit = "apple";
 
-switch(myFruit){
-    case "apple":
-        console.log('Apple!');
-        break;
-    case "orange":
-        console.log('Im orange');
-        break;
-    case "banana":
-        console.log('banana');;
-        break;
-    default:
-        console.log("I didn't understand");       
 
+let label = ["second", "minute", "hour", "day", "seconds", "minutes", "hours", "days"];
+let isSingularV = [];
+let isSingularL = [];
+let flagSwitch = [];
+let value3=0, label3 = "minutes";
+
+let minutesConvert = (timeV, timeL) => {
+
+    for (let j = 0; j < timeL.length; j++) {
+
+        switch (timeL[j]) {
+            case "second":
+                value3  += timeV[j] / 60;
+                break;
+            case "minute":
+                value3 += timeV[j];
+                break;
+            case "hour":
+                value3 += timeV[j] * 60;
+                break;
+            case "day":
+                value3 += timeV[j] * 1440;
+                break;
+            case "seconds":
+                value3 += timeV[j] / 60;
+                break;
+            case "minutes":
+                value3 += timeV[j];
+                break;
+            case "hours":
+                value3 += timeV[j] * 60;
+                break;
+            case "days":
+                value3 += timeV[j] * 1440;
+                break;
+        }
+        
+    }
+    return [value3,label3];
 }
 
- */
+let timeAdder = (a, b, c, d) => {
 
- const timeAdder = (value1,label1,value2,label2) => {
-
-    const values = [value1,value2];
-    const labels = [label1,label2];
-    const validValues = ["seconds", "minutes", "hours", "days", "second", "minute", "hour", "day"];
-    let matches = 0;
+    const values = [a, c];
+    const labels = [b, d];
+    let vl = 0;
+    let vv = 0;
 
     const validateLabels = () => {
-        labels.forEach(function (element){
-            for(let i=0;i<validValues.length;i++){
-                element === validValues[i] ? ++matches : matches;
-            }
-            
-        })
-
-        return matches == 2 ? true : console.log("Incorrect: Verify labels's values");
-        
+        labels.forEach(x => {
+            label.forEach(y => {
+                x === y ? ++vl : false;
+            })
+        });
+        return vl === 2 ? true : false
     }
 
     const validateValues = () => {
-        values.forEach(element){
-            
+        values.forEach(x => {
+            Number.isInteger(x) ? x > 0 ? ++vv : false : false
+        })
+        return vv == 2 ? true : false
+    }
+
+    const isValueSingular = x => {
+        return x === 1 ? true : false;
+    }
+    const isLabelSingular = y => {
+        switch (y) {
+            case "second":
+                return true;
+                break;
+            case "minute":
+                return true;
+                break;
+            case "hour":
+                return true;
+                break;
+            case "day":
+                return true;
+                break;
+            default:
+                return false;
+                break;
         }
     }
 
-    validateLabels() ? true : console.log("Incorrect: Verify labels's values");
-    console.log(validateLabels());
-     
-     
 
-    /*const dataValidate = (originalArray,compareArray) => {
-        
-        originalArray[1] === compareArray.forEach((element => element) ? console.log("Hay match") : console.log("no hay match")
-        //originalArray[3] === compareArray[i] ? matches++ : console.log(`${originArray[3]} is invalid`);
-            
+
+    if (validateValues() === validateLabels()) {
+
+        for (let i = 0; i < values.length; i++) {
+            isSingularV[i] = isValueSingular(values[i]);
+        }
+
+        for (let i = 0; i < labels.length; i++) {
+            isSingularL[i] = isLabelSingular(labels[i]);
+        }
+
+        if (isSingularV[0] === isSingularL[0]) {
+            flagSwitch[0] = true;
+        } else {
+            console.log(`${values[0]} & ${labels[0]} impossible combined`);
+
+        }
+        if (isSingularV[1] === isSingularL[1]) {
+            flagSwitch[1] = true;
+        } else {
+            console.log(`${values[1]} & ${labels[1]} impossible combined`);
+        }
+
+
+        if (flagSwitch[0] === flagSwitch[1]) {
+
+            console.log(minutesConvert(values,labels));
+             
+        }
+
+
+    } else {
+        console.log("Invalid values or labels");
+
     }
-    
-    dataValidate(valueorlabel,validValues);
-     /*
-    const isPlural = valueorlabel => {
-        let isNumber, isString;
-        Number.isInteger(valueorlabel) ? isNumber = true : isString = true;
 
-        
-    }
+    //validateValues() === validateLabels() ? console.log("true") : console.log('Invalid values or labels');
 
-       
-     label1.charAt(label1.length-1) === "s" || label2.charAt(label2.length-1) === "s" ? labelPlural = ["seconds","minutes","hours","days"]:labelSingular = ["second","minute","hour","day"];
-     
-     switch(labelPlural){
 
-     }
-     
-     console.log(labelPlural[1]);
-     */
- }
-
- timeAdder(1,"minutes",3,"minutes");
+}
+timeAdder(5, "hours", 5, "days");
